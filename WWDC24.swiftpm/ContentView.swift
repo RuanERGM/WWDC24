f773040755d5759d6ab2ebe8a46b4d77a7bb7728
Ruan Eleuterio <ruaneleuterio161@gmail.com>
2024-02-26 02:31:38 -0400

    feat: finish

--- WWDC24.swiftpm/ContentView.swift
+++ WWDC24.swiftpm/ContentView.swift
@@ -1,14 +1,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var timeRemaining = 30
+    @State private var timer: Timer?
+    @State private var isRunning: Bool = false
+    @State private var index = 0
+    @State private var notes = ["C","D","E","F","G","A","B","Cm","Dm","Em","Fm","Gm","Am","Bm"]
+    
+    
     
     var body: some View {
+        
         ZStack{
             
-            Color.black
+            
+            Image(.background)
+                .padding(.top,-400)
+            
+            
+            
+            
             VStack {
+                Text("The objective of this playgorund is to teach some basic piano chords. The chord appears in the middle of the screen and the corresponding keys are highlighted to be pressed together.")
+                    .padding()
+                    .foregroundColor(.orange)
+                    .fontWeight(.bold)
+                    .font(.system(size: 30))
+                    .padding(.top,180)
+
+                
+                
+                Text(notes[index])
+                    .foregroundColor(.orange)
+                    .fontWeight(.bold)
+                    .font(.system(size: 100))
+                    .padding(.top,100)
+                
                 Spacer()
                 Image(.piano)
+                    .resizable()
+                    .padding(.bottom,-22)
+                    .frame(width: 1024,height: 401)
+                    
                 
             }
             ZStack{
@@ -18,152 +51,217 @@ struct ContentView: View {
                     
                     
                     Rectangle()
-                        .fill(Color.gray)
-                        .frame(width: 70,height: 399)
+                        .fill(index == 0 || index == 7 ? Color.blue.opacity(0.3) : Color.white)
+                        .frame(width: 50,height: 399)
                         .onTapGesture {
-                            SoundManager.shared.playSound(sound: "C", type: ".mp3")
+                            SoundManager.shared.C(sound: "C", type: ".mp3")
                             
                         }
-                        .position(x:50,y: 1130)
+                        .position(x:40,y: 1145)
                     
                     Rectangle()
-                        .fill(Color.gray)
-                        .frame(width: 70,height: 399)
+                        .fill(index == 1 || index == 8 ? Color.blue.opacity(0.3) : Color.white)
+                        .frame(width: 50,height: 399)
                         .onTapGesture {
-                            SoundManager.shared.playSound(sound: "D", type: ".mp3")
+                            SoundManager.shared.D(sound: "D", type: ".mp3")
                             
                         }
-                        .position(CGPoint(x: -156, y: 1130))
+                        .position(CGPoint(x: -186, y: 1145))
                     
                     Rectangle()
-                        .fill(Color.gray)
-                        .frame(width: 70,height: 399)
+                        .fill(index == 2 || index == 9 || index == 0 ? Color.blue.opacity(0.3) : Color.white)                   .frame(width: 50,height: 399)
                         .onTapGesture {
-                            SoundManager.shared.playSound(sound: "E", type: ".mp3")
+                            SoundManager.shared.E(sound: "E", type: ".mp3")
                             
                         }
-                        .position(x:-360,y: 1130)
-
+                        .position(x:-425,y: 1145)
+                    
                 }
                 HStack {
                     
                     
                     Rectangle()
-                        .fill(Color.gray)
-                        .frame(width: 70,height: 399)
+                        .fill(index == 3 || index == 8 || index == 10 ? Color.blue.opacity(0.3) : Color.white)                       .frame(width: 50,height: 399)
                         .onTapGesture {
-                            SoundManager.shared.playSound(sound: "F", type: ".mp3")
+                            SoundManager.shared.F(sound: "F", type: ".mp3")
                             
                         }
-                        .position(x:440,y: 1130)
+                        .position(x:355,y: 1145)
                     
                     Rectangle()
-                        .fill(Color.gray)
-                        .frame(width: 70,height: 399)
+                        .fill(index == 0 || index == 4 || index == 7 || index == 9 || index == 11 ? Color.blue.opacity(0.3) : Color.white) .frame(width: 50,height: 399)
                         .onTapGesture {
-                            SoundManager.shared.playSound(sound: "G", type: ".mp3")
+                            SoundManager.shared.G(sound: "G", type: ".mp3")
                             
                         }
-                        .position(CGPoint(x: 315, y: 1130))
+                        .position(CGPoint(x: 205, y: 1145))
                     
                     Rectangle()
-                        .fill(Color.gray)
-                        .frame(width: 70,height: 399)
+                        .fill(index == 1 || index == 3 || index == 5 || index == 8 || index == 12 ? Color.blue.opacity(0.3) : Color.white)                        .frame(width: 50,height: 399)
                         .onTapGesture {
-                            SoundManager.shared.playSound(sound: "A", type: ".mp3")
+                            SoundManager.shared.A(sound: "A", type: ".mp3")
                             
                         }
-                        .position(x:183,y: 1130)
+                    
+                        .position(x:45,y: 1145)
                     
                     Rectangle()
-                        .fill(Color.gray)
-                        .frame(width: 70,height: 399)
+                        .fill(index == 2 || index == 4 || index == 6 || index == 9 || index == 13 ? Color.blue.opacity(0.3) : Color.white)                        .frame(width: 50,height: 399)
                         .onTapGesture {
-                            SoundManager.shared.playSound(sound: "B", type: ".mp3")
+                            SoundManager.shared.B(sound: "B", type: ".mp3")
                             
                         }
-                        .position(x:55,y: 1130)
+                        .position(x:-100,y: 1145)
                     
-  
+                    
                 }
                 HStack {
                     
                     
                     Rectangle()
-                        .fill(Color.gray)
-                        .frame(width: 70,height: 399)
+                        .fill(index == 3 || index == 10 || index == 12 ? Color.blue.opacity(0.3) : Color.white)                     .frame(width: 50,height: 399)
                         .onTapGesture {
-                            SoundManager.shared.playSound(sound: "C3", type: ".mp3")
+                            SoundManager.shared.C3(sound: "C3", type: ".mp3")
                             
                         }
-                        .position(x:950,y: 1130)
+                        .position(x:760,y: 1145)
+                    
+                    Rectangle()
+                        .fill(index == 4 || index == 11 || index == 13 ? Color.blue.opacity(0.3) : Color.white)                         .frame(width: 50,height: 399)
+                        .onTapGesture {
+                            SoundManager.shared.D3(sound: "D3", type: ".mp3")
+                            
+                        }
+                        .position(x:354,y: 1145)
+                    
+                    
                 }
+                Rectangle()
+                    .fill(index == 5 || index == 12 ? Color.blue.opacity(0.3) : Color.white)                      .frame(width: 50,height: 399)
+                    .onTapGesture {
+                        SoundManager.shared.E3(sound: "E3", type: ".mp3")
+                        
+                    }
+                    .position(x:980,y: 1145)
                 HStack{
                     Rectangle()
-                        .fill(Color.blue)
+                        .fill( .black)
                         .frame(width: 30,height: 200)
                         .onTapGesture {
-                            SoundManager.shared.playSound(sound: "C#", type: ".mp3")
+                            SoundManager.shared.Csus(sound: "C#", type: ".mp3")
                             
                         }
-                        .position(x:125,y: 1050)
+                        .position(x:105,y: 1065)
                     Rectangle()
-                        .fill(Color.blue)
+                        .fill(index == 7 ? Color.red.opacity(0.6) : Color.black)
                         .frame(width: 30,height: 200)
                         .onTapGesture {
-                            SoundManager.shared.playSound(sound: "D#", type: ".mp3")
+                            SoundManager.shared.Dsus(sound: "D#", type: ".mp3")
                             
                         }
-                        .position(x:-263,y: 1050)
+                        .position(x:-307,y: 1065)
                     
                     
                 }
                 
                 HStack{
                     Rectangle()
-                        .fill(Color.blue)
+                        .fill(index == 1 || index == 6 || index == 13 ? Color.red.opacity(0.6) : Color.black)
                         .frame(width: 30,height: 200)
                         .onTapGesture {
-                            SoundManager.shared.playSound(sound: "F#", type: ".mp3")
+                            SoundManager.shared.Fsus(sound: "F#", type: ".mp3")
                             
                         }
-                        .position(x:510,y: 1050)
+                        .position(x:410,y: 1065)
                     
                     Rectangle()
-                        .fill(Color.blue)
-                        .frame(width: 30,height: 200)
+                        .fill(index == 2 || index == 10 ? Color.red.opacity(0.6) : Color.black)  .frame(width: 30,height: 200)
                         .onTapGesture {
-                            SoundManager.shared.playSound(sound: "G#", type: ".mp3")
+                            SoundManager.shared.Gsus(sound: "G#", type: ".mp3")
                             
                         }
-                        .position(x:121,y: 1050)
+                        .position(x:0,y: 1065)
                 }
                 
                 HStack{
                     Rectangle()
-                        .fill(Color.blue)
-                        .frame(width: 30,height: 200)
+                        .fill(index == 11 ? Color.red.opacity(0.6) : Color.black)                        .frame(width: 30,height: 200)
                         .onTapGesture {
-                            SoundManager.shared.playSound(sound: "A#", type: ".mp3")
+                            SoundManager.shared.Asus(sound: "A#", type: ".mp3")
                             
                         }
-                        .position(x:760,y: 1050)
+                        .position(x:615,y: 1065)
                     
                     Rectangle()
-                        .fill(Color.blue)
-                        .frame(width: 30,height: 200)
+                        .fill(index == 5 ? Color.red.opacity(0.6) : Color.black)                       .frame(width: 30,height: 200)
                         .onTapGesture {
-                            SoundManager.shared.playSound(sound: "C3#", type: ".mp3")
+                            SoundManager.shared.C3sus(sound: "C3#", type: ".mp3")
                             
                         }
-                        .position(x:500,y: 1050)
+                        .position(x:305,y: 1065)
+                    
+                    
                 }
                 
+                HStack{
+                    Rectangle()
+                        .fill(index == 6 ? Color.red.opacity(0.6) : Color.black)                         .frame(width: 30,height: 200)
+                        .onTapGesture {
+                            SoundManager.shared.D3sus(sound: "D3#", type: ".mp3")
+                            
+                        }
+                        .position(x:925,y: 1065)
+                }
+                
+                
+                
+            }
             
+
+        }
+        .onAppear(){
+            startTimer()
+        }
+        .onChange(of: timeRemaining){ newIndex in
+            if(timeRemaining % 2 == 0 && timeRemaining != 0){
+                
+                if index < 13{
+                    index += 1
+                }
+                else {
+                    index = 0
+                }
             }
             
         }
         
+        
     }
+    
+    private func formattedTime () -> String{
+        let minutes = Int (timeRemaining) / 60
+        let second = Int (timeRemaining) % 60
+        return String (format: "%02d:%02d", minutes, second)
+    }
+    
+    private func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1,repeats: true){_ in
+            if timeRemaining < 30 && timeRemaining >= 0{
+                timeRemaining += 1
+            }
+            else{
+                stopTimer ()
+            }
+        }
+    }
+    
+    private func stopTimer() {
+        isRunning = false
+        timer?.invalidate()
+        timeRemaining = 0
+        startTimer()
+    }
+    
+    
 }
 
--- WWDC24.swiftpm/SoundManager.swift
+++ WWDC24.swiftpm/SoundManager.swift
@@ -14,6 +14,25 @@ public class SoundManager{
     @Published var tutorial: Bool = true
     static let shared = SoundManager()
     var player: AVAudioPlayer?
+    var C: AVAudioPlayer?
+    var Csus: AVAudioPlayer?
+    var D: AVAudioPlayer?
+    var Dsus: AVAudioPlayer?
+    var E: AVAudioPlayer?
+    var F: AVAudioPlayer?
+    var Fsus: AVAudioPlayer?
+    var G: AVAudioPlayer?
+    var Gsus: AVAudioPlayer?
+    var A: AVAudioPlayer?
+    var Asus: AVAudioPlayer?
+    var B: AVAudioPlayer?
+    var C3: AVAudioPlayer?
+    var C3sus: AVAudioPlayer?
+    var D3: AVAudioPlayer?
+    var D3sus: AVAudioPlayer?
+    var E3: AVAudioPlayer?
+
+
     
     func playSound(sound: String,type: String) {
         guard let path = Bundle.main.path(forResource: sound, ofType: type) else {
@@ -29,6 +48,254 @@ public class SoundManager{
             print("ERROR")
         }
     }
+    
+    func C(sound: String,type: String) {
+        guard let path = Bundle.main.path(forResource: sound, ofType: type) else {
+            print("Resource not found: \(sound)")
+            return
+        }
+
+        do {
+            C = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            C?.play()
+            
+        } catch {
+            print("ERROR")
+        }
+    }
+    func Csus(sound: String,type: String) {
+        guard let path = Bundle.main.path(forResource: sound, ofType: type) else {
+            print("Resource not found: \(sound)")
+            return
+        }
+
+        do {
+            Csus = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            Csus?.play()
+            
+        } catch {
+            print("ERROR")
+        }
+    }
+    func D(sound: String,type: String) {
+        guard let path = Bundle.main.path(forResource: sound, ofType: type) else {
+            print("Resource not found: \(sound)")
+            return
+        }
+
+        do {
+            D = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            D?.play()
+            
+        } catch {
+            print("ERROR")
+        }
+    }
+    func Dsus(sound: String,type: String) {
+        guard let path = Bundle.main.path(forResource: sound, ofType: type) else {
+            print("Resource not found: \(sound)")
+            return
+        }
+
+        do {
+            Dsus = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            Dsus?.play()
+            
+        } catch {
+            print("ERROR")
+        }
+    }
+    func E(sound: String,type: String) {
+        guard let path = Bundle.main.path(forResource: sound, ofType: type) else {
+            print("Resource not found: \(sound)")
+            return
+        }
+
+        do {
+            E = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            E?.play()
+            
+        } catch {
+            print("ERROR")
+        }
+    }
+    
+    func F(sound: String,type: String) {
+        guard let path = Bundle.main.path(forResource: sound, ofType: type) else {
+            print("Resource not found: \(sound)")
+            return
+        }
+
+        do {
+            F = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            F?.play()
+            
+        } catch {
+            print("ERROR")
+        }
+    }
+    func Fsus(sound: String,type: String) {
+        guard let path = Bundle.main.path(forResource: sound, ofType: type) else {
+            print("Resource not found: \(sound)")
+            return
+        }
+
+        do {
+            Fsus = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            Fsus?.play()
+            
+        } catch {
+            print("ERROR")
+        }
+    }
+    func G(sound: String,type: String) {
+        guard let path = Bundle.main.path(forResource: sound, ofType: type) else {
+            print("Resource not found: \(sound)")
+            return
+        }
+
+        do {
+            G = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            G?.play()
+            
+        } catch {
+            print("ERROR")
+        }
+    }
+    
+    func Gsus(sound: String,type: String) {
+        guard let path = Bundle.main.path(forResource: sound, ofType: type) else {
+            print("Resource not found: \(sound)")
+            return
+        }
+
+        do {
+            Gsus = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            Gsus?.play()
+            
+        } catch {
+            print("ERROR")
+        }
+    }
+    
+    func A(sound: String,type: String) {
+        guard let path = Bundle.main.path(forResource: sound, ofType: type) else {
+            print("Resource not found: \(sound)")
+            return
+        }
+
+        do {
+            A = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            A?.play()
+            
+        } catch {
+            print("ERROR")
+        }
+    }
+    
+    func Asus(sound: String,type: String) {
+        guard let path = Bundle.main.path(forResource: sound, ofType: type) else {
+            print("Resource not found: \(sound)")
+            return
+        }
+
+        do {
+            Asus = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            Asus?.play()
+            
+        } catch {
+            print("ERROR")
+        }
+    }
+    func B(sound: String,type: String) {
+        guard let path = Bundle.main.path(forResource: sound, ofType: type) else {
+            print("Resource not found: \(sound)")
+            return
+        }
+
+        do {
+            B = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            B?.play()
+            
+        } catch {
+            print("ERROR")
+        }
+    }
+    func C3(sound: String,type: String) {
+        guard let path = Bundle.main.path(forResource: sound, ofType: type) else {
+            print("Resource not found: \(sound)")
+            return
+        }
+
+        do {
+            C3 = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            C3?.play()
+            
+        } catch {
+            print("ERROR")
+        }
+    }
+    
+    func C3sus(sound: String,type: String) {
+        guard let path = Bundle.main.path(forResource: sound, ofType: type) else {
+            print("Resource not found: \(sound)")
+            return
+        }
+
+        do {
+            C3sus = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            C3sus?.play()
+            
+        } catch {
+            print("ERROR")
+        }
+    }
+    
+    func D3(sound: String,type: String) {
+        guard let path = Bundle.main.path(forResource: sound, ofType: type) else {
+            print("Resource not found: \(sound)")
+            return
+        }
+
+        do {
+            D3 = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            D3?.play()
+            
+        } catch {
+            print("ERROR")
+        }
+    }
+    
+    func E3(sound: String,type: String) {
+        guard let path = Bundle.main.path(forResource: sound, ofType: type) else {
+            print("Resource not found: \(sound)")
+            return
+        }
+
+        do {
+            E3 = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            E3?.play()
+            
+        } catch {
+            print("ERROR")
+        }
+    }
+    
+    func D3sus(sound: String,type: String) {
+        guard let path = Bundle.main.path(forResource: sound, ofType: type) else {
+            print("Resource not found: \(sound)")
+            return
+        }
+
+        do {
+            D3sus = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            D3sus?.play()
+            
+        } catch {
+            print("ERROR")
+        }
+    }
+    
    
     
     func stop(){
